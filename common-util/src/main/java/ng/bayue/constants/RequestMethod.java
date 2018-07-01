d8008d55db23be32f240be79d28ee4ecdbd00cc7
fengyts <fengyts@163.com>
2018-07-01 10:51:56 +0800

    项目结构调整

---
+++ common-util/src/main/java/ng/bayue/constants/RequestMethod.java
@@ -0,0 +1,5 @@
+package ng.bayue.constants;
+
+public enum RequestMethod {
+	GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE;
+}
---
+++ common-util/src/main/java/ng/bayue/constants/SymbolConstant.java
@@ -0,0 +1,22 @@
+package ng.bayue.constants;
+
+/**
+ * <pre>
+ * 常用符号常量
+ * </pre>
+ *
+ * @author fengyts
+ * @version $Id: SymbolConstant.java, v 0.1 2018年6月30日 下午2:35:56 fengyts Exp $
+ */
+public final class SymbolConstant {
+	
+	/** 逗号 */
+	public static final String COMMA = ",";
+	/** 冒号 */
+	public static final String COLON = ":";
+	/** 下划线 */
+	public static final String UNDERSCORE = "_";
+	/** 横线 */
+	public static final String HORIZONTAL_LINE  = "-";
+
+}
